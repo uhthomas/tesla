90bb074c3ef3db7071964e6438504bde2558b48a
Thomas <9749173+uhthomas@users.noreply.github.com>
2021-02-25 22:03:30 +0000

    import code from uhthomas/tesla_export, expose options for creating an OAuth2 client

--- option.go
+++ option.go
@@ -1,3 +1,27 @@
 package tesla
 
-type Option func(*Service) error
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Option func(ctx context.Context, s *Service) error
+
+func Client(c *http.Client) Option {
+	return func(ctx context.Context, s *Service) error {
+		s.c = c
+		return nil
+	}
+}
+
+func OAuth2(configPath, tokenPath string) Option {
+	return func(ctx context.Context, s *Service) error {
+		c, err := newOAuth2Client(ctx, configPath, tokenPath)
+		if err != nil {
+			return fmt.Errorf("new oauth2 client: %w", err)
+		}
+		s.c = c
+		return nil
+	}
+}
--- service.go
+++ service.go
@@ -1,17 +1,29 @@
 package tesla
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+	"net/url"
+)
 
 type Service struct {
-	c *http.Client
+	c         *http.Client
+	baseURL   *url.URL
+	userAgent string
 }
 
-// New creates a new Tesla service client. The provided HTTP client must be
-// using an OAuth2 transport.
-func New(c *http.Client, opts ...Option) (*Service, error) {
-	s := &Service{c: c}
+// New creates a new Tesla service client.
+func New(ctx context.Context, opts ...Option) (*Service, error) {
+	s := &Service{
+		baseURL: &url.URL{
+			Scheme: "https",
+			Host:   "owner-api.teslamotors.com",
+			Path:   "api/1",
+		},
+		userAgent: "uhthomas/tesla",
+	}
 	for _, opt := range opts {
-		if err := opt(s); err != nil {
+		if err := opt(ctx, s); err != nil {
 			return nil, err
 		}
 	}
