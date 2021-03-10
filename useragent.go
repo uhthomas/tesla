1caf33b692d9258ef27505b335ee3aa8869c1e8d
Thomas <9749173+uhthomas@users.noreply.github.com>
2021-03-10 22:58:44 +0000

    use hex

--- useragent.go
+++ useragent.go
@@ -2,15 +2,15 @@ package tesla
 
 import (
 	"crypto/rand"
-	"encoding/base64"
 	"encoding/binary"
+	"encoding/hex"
 	"io"
 	"strconv"
 	"strings"
 )
 
 func userAgent() string {
-	const prefixBytes = 9
+	const prefixBytes = 10
 	var buf [prefixBytes + 8]byte
 	if _, err := io.ReadFull(rand.Reader, buf[:]); err != nil {
 		// I know.
@@ -18,7 +18,7 @@ func userAgent() string {
 	}
 
 	var b strings.Builder
-	b.WriteString(base64.RawURLEncoding.EncodeToString(buf[:prefixBytes]))
+	b.WriteString(hex.EncodeToString(buf[:prefixBytes]))
 	b.WriteRune('/')
 	b.Write(strconv.AppendUint(nil, binary.BigEndian.Uint64(buf[prefixBytes:]), 10))
 	return b.String()
